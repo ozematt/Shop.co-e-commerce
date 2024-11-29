6479d7728bd455c22458d3e003ffbac0ec43d7eb
Mateusz Obirek <neutralnie7@gmail.com>
2024-11-29 10:35:46 +0100

    added navbar media queries

--- app/src/sections/Nav.tsx
+++ app/src/sections/Nav.tsx
@@ -1,15 +1,30 @@
 import { navLinks } from "../constants";
 import cartIcon from "../assets/Cart.png";
 import userIcon from "../assets/Avatar.png";
+import hamburger from "../assets/Hamburger.png";
+import lupeIcon from "../assets/Lupe_icon.png";
 
 const Nav = () => {
   return (
-    <nav className="px-[100px] flex items-center h-[96px]">
-      <div className="font-integralCFBold text-[32px]">
+    <nav className="px-4 sm:px-[100px] flex items-center max-[838px]:justify-between h-[96px]">
+      <div className="flex items-center">
         {" "}
-        <a href="/">shop.co</a>{" "}
+        <img
+          src={hamburger}
+          alt="hamburger menu"
+          width={24}
+          height={24}
+          className="mr-4 min-w-[24px] sm:min-w-[30px] min-[1192px]:hidden"
+        />
+        <a
+          href="/"
+          className="font-integralCFBold text-[25px] sm:text-[32px] mb-[7px]"
+        >
+          shop.co
+        </a>
       </div>
-      <ul className="flex justify-center items-center gap-[24px] min-w-[390px] pl-[40px] pt-2 font-satoshi font-normal">
+
+      <ul className="max-[1192px]:hidden flex justify-center items-center gap-[24px] min-w-[390px] pl-[40px]  font-satoshi font-normal">
         {navLinks.map((link, index) => (
           <li key={index} className="py-3">
             <a href={link.href} className="hover:opacity-60">
@@ -22,17 +37,28 @@ const Nav = () => {
       <input
         type="text"
         placeholder=" Search for products..."
-        className="w-full max-w-[577px] h-[48px] ml-[40px] mt-1 pl-[57px] rounded-full bg-grayBG bg-lupe-icon bg-no-repeat bg-[center_left_1.5rem]"
+        className="hidden min-[838px]:block w-full max-w-[577px] h-[48px] ml-[40px] mt-1 pl-[57px] rounded-full bg-grayBG bg-lupe-icon bg-no-repeat bg-[center_left_1.5rem]"
       />
-      <div className="flex items-center gap-[14px] ml-[40px]">
+      <div className="flex items-center gap-[14px] ml-[40px] min-w-[62px]">
+        <img
+          src={lupeIcon}
+          alt="lupe icon"
+          width={24}
+          height={24}
+          className="hidden max-[837px]:block"
+        />
         <img
           src={cartIcon}
           alt="cart icon"
+          width={24}
+          height={24}
           className="cursor-pointer hover:opacity-60"
         />
         <img
           src={userIcon}
           alt="user icon"
+          width={24}
+          height={24}
           className="cursor-pointer hover:opacity-60"
         />
       </div>
