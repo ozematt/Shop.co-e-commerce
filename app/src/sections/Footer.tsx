37e6ce606bf1e72a0bc58b44939621755abd94a8
Mateusz Obirek <neutralnie7@gmail.com>
2024-12-02 17:34:27 +0100

    added social icons

--- app/src/sections/Footer.tsx
+++ app/src/sections/Footer.tsx
@@ -1,5 +1,15 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <section>
+      <div>
+        <h3>Shop.co</h3>
+        <p>
+          We offer products that match your style and make you proud — for
+          everyone, everywhere.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Footer;
