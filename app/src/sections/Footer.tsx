2af6aff58a90de878cdda0540c34c306b51d66aa
Mateusz Obirek <neutralnie7@gmail.com>
2024-12-03 09:18:03 +0100

    added footer media queries

--- app/src/sections/Footer.tsx
+++ app/src/sections/Footer.tsx
@@ -4,13 +4,13 @@ import pay from "../assets/Pay.png";
 const Footer = () => {
   return (
     <section className="px-4 sm:px-[80px] mt-[50px] relative">
-      <div className="flex">
+      <div className="flex max-[820px]:flex-wrap">
         {" "}
-        <div>
+        <div className="">
           <h3 className="font-integralCFBold text-[34px] leading-[28px]">
             Shop.co
           </h3>
-          <p className="font-satoshi text-[14px] opacity-60 w-full max-w-[248px] mt-[25px]">
+          <p className="font-satoshi text-[14px] opacity-60 w-full md:max-w-[248px] mt-[25px]">
             We offer products that match your style and make you proud — for
             everyone, everywhere.
           </p>
@@ -26,17 +26,20 @@ const Footer = () => {
             ))}
           </div>
         </div>
-        <div className="flex flex-wrap">
+        <div className="grid grid-cols-2 ld:grid-cols-3 xl:grid-cols-4 max-[820px]:pt-[24px]">
           {" "}
           {footerData.map((item) => (
-            <div key={item.title} className="pl-[110px]">
-              <p className="font-satoshi font-medium uppercase tracking-[.35rem]">
+            <div
+              key={item.title}
+              className="max-[820px]:odd:pl-0 pl-[40px] min-[820px]:pl-[110px] pb-6"
+            >
+              <p className="font-satoshi max-sm:text-[14px] font-medium uppercase tracking-[.35rem]">
                 {item.title}
               </p>
               {item.items.map((element) => (
                 <p
                   key={element}
-                  className="pt-4 font-satoshi opacity-60 hover:opacity-30 cursor-pointer"
+                  className="pt-2 sm:pt-4 max-sm:text-[12px] font-satoshi opacity-60 hover:opacity-30 cursor-pointer"
                 >
                   {element}
                 </p>
@@ -46,12 +49,16 @@ const Footer = () => {
         </div>
       </div>
       <div className="border-b-2 mt-[50px]" />
-      <div className="flex justify-between items-center">
+      <div className="flex flex-wrap justify-center sm:justify-between items-center">
         {" "}
-        <p className="font-satoshi text-[14px] opacity-60 mt-[25px] mb-[88px]">
+        <p className="font-satoshi text-[14px]  opacity-60 mt-[25px] sm:mb-[88px]">
           Shop.co © 2000-2024, All Rights Reserved
         </p>
-        <img src={pay} alt="payment method" />
+        <img
+          src={pay}
+          alt="payment method"
+          className="max-sm:p-2 sm:mb-[60px]"
+        />
       </div>
       {/* <div className="bg-grayBG w-full max-w-[1440] h-full min-h-[500px] absolute bottom-0 left-0 z-[-1]" /> */}
     </section>
