0ac01d855859cda875712630ee0e71a7c2e7a18e
Mateusz Obirek <neutralnie7@gmail.com>
2024-12-03 18:43:24 +0100

    added filter window logic

--- app/src/components/Shop.tsx
+++ app/src/components/Shop.tsx
@@ -1,8 +1,32 @@
 import Breadcrumbs from "./Breadcrumbs";
 import settings from "../assets/Settings.png";
 import arrow from "../assets/Arrow down.png";
+import { useQuery } from "@tanstack/react-query";
+import fetchCategoriesList from "../api/queries/categories";
+import { useState } from "react";
 
 const Shop = () => {
+  //
+  ////DATA
+  const [priceOpen, setPriceOpen] = useState(true);
+  const [categoryOpen, setCategoryOpen] = useState(true);
+
+  const { data: categories } = useQuery({
+    queryKey: ["categories"],
+    queryFn: fetchCategoriesList,
+  });
+
+  ////LOGIC
+  const handleFiltersOpen = () => {
+    setCategoryOpen(true);
+    setPriceOpen(true);
+    if (priceOpen && categoryOpen) {
+      setCategoryOpen(false);
+      setPriceOpen(false);
+    }
+  };
+
+  ////UI
   return (
     <section className="px-4 sm:px-[100px] max-container">
       <div className="border-b-2" />
@@ -17,16 +41,43 @@ const Shop = () => {
             height={24}
             alt="settings"
             className="-rotate-90 opacity-60"
+            onClick={handleFiltersOpen}
           />
         </div>
         <div className="border-t-2 pb-6" />
+
+        <div className="flex justify-between items-center">
+          <p className="font-satoshi font-bold text-[20px]">Category</p>
+          <img
+            src={arrow}
+            width={20}
+            height={20}
+            alt="arrow"
+            onClick={() => setCategoryOpen(!categoryOpen)}
+            className="opacity-60 hover:opacity-100 cursor-pointer"
+            style={{
+              transform: `rotate(${!priceOpen ? "180deg" : "0deg"})`,
+            }}
+          />
+        </div>
+
         <div className="pb-6">
-          <p>categiries</p>
-          <p>categiries</p>
-          <p>categiries</p>
-          <p>categiries</p>
+          {categoryOpen &&
+            categories?.map((category) => (
+              <div
+                key={category}
+                className="flex items-center justify-between first:pt-6"
+              >
+                {" "}
+                <p className="font-satoshi pb-2 opacity-60 hover:opacity-100 cursor-pointer">
+                  {category.charAt(0).toUpperCase() + category.slice(1)}
+                </p>
+                <img src={arrow} alt="arow" className="-rotate-90" />
+              </div>
+            ))}
         </div>
         <div className="border-t-2 pb-6" />
+        {/* PRICE */}
         <div className="flex justify-between items-center pb-6">
           <p className="font-satoshi font-bold text-[20px] ">Price</p>
           <img
@@ -34,21 +85,28 @@ const Shop = () => {
             width={20}
             height={20}
             alt="arrow"
-            className=" opacity-60"
+            onClick={() => setPriceOpen(!priceOpen)}
+            className="opacity-60 hover:opacity-100 cursor-pointer"
+            style={{
+              transform: `rotate(${!priceOpen ? "180deg" : "0deg"})`,
+            }}
           />
         </div>
-        <div className="flex gap-2 pb-6">
-          <input
-            type="text"
-            className="ring-1 ring-black ring-opacity-20 focus:ring-black  focus:outline-none  w-full max-w-[120px] h-7 placeholder:text-sm  pl-2 rounded-sm "
-            placeholder="from:"
-          />
-          <input
-            type="text"
-            className="ring-1 ring-black ring-opacity-20 focus:ring-black  focus:outline-none w-full max-w-[120px] h-7 placeholder:text-sm pl-2 rounded-sm"
-            placeholder="to:"
-          />
-        </div>
+        {priceOpen && (
+          <div className="flex gap-2 pb-6">
+            <input
+              type="text"
+              className="ring-1 ring-black ring-opacity-20 focus:ring-black  focus:outline-none  w-full max-w-[120px] h-7 placeholder:text-sm  pl-2 rounded-sm "
+              placeholder="from:"
+            />
+            <input
+              type="text"
+              className="ring-1 ring-black ring-opacity-20 focus:ring-black  focus:outline-none w-full max-w-[120px] h-7 placeholder:text-sm pl-2 rounded-sm"
+              placeholder="to:"
+            />
+          </div>
+        )}
+
         <div className="border-t-2 pb-6" />
         <button className="w-full text-[14px] px-[86px] py-[15px] bg-black rounded-full text-white transition ease-in-out duration-100 hover:scale-95">
           Apply Filter
