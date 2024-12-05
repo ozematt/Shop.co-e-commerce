5ec078d393d56e35410bcf2365cf7716408697a7
Mateusz Obirek <neutralnie7@gmail.com>
2024-12-05 18:45:27 +0100

    added comments

--- app/src/lib/helpers/generatePagination.ts
+++ app/src/lib/helpers/generatePagination.ts
@@ -1,34 +1,34 @@
 export const generatePagination = (currentPage: number, totalPages: number) => {
   const pageNumbers = [];
 
-  // Jeśli jest mniej niż 5 stron, pokaż wszystkie
+  // if there are less than 5 pages, show all
   if (totalPages <= 5) {
     for (let i = 1; i <= totalPages; i++) {
       pageNumbers.push(i);
     }
   } else {
-    // Dodaj pierwszą stronę
+    // add first page
     pageNumbers.push(1);
 
-    // Dodaj kropki, jeśli trzeba
+    // add dots if needed
     if (currentPage > 3) {
       pageNumbers.push("...");
     }
 
-    // Dodaj strony wokół obecnej strony
-    const start = Math.max(currentPage - 1, 2); // minimalnie 2
-    const end = Math.min(currentPage + 1, totalPages - 1); // maksymalnie przed ostatnią stroną
+    // add pages around current page
+    const start = Math.max(currentPage - 1, 2); // minimum 2
+    const end = Math.min(currentPage + 1, totalPages - 1); // at most before the last page
 
     for (let i = start; i <= end; i++) {
       pageNumbers.push(i);
     }
 
-    // Dodaj kropki, jeśli trzeba
+    // add dots if needed
     if (currentPage < totalPages - 2) {
       pageNumbers.push("...");
     }
 
-    // Dodaj ostatnią stronę
+    // add last page
     pageNumbers.push(totalPages);
   }
 
