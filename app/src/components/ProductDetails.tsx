5ad146c8449dc9c4d2e4fd8ccd82d8a72ef46823
Mateusz Obirek <neutralnie7@gmail.com>
2024-12-12 12:14:51 +0100

    added changing details logic

--- app/src/components/ProductDetails.tsx
+++ app/src/components/ProductDetails.tsx
@@ -1,8 +1,11 @@
 // import { Breadcrumbs } from "@mui/material";
 import { Rating } from "@mui/material";
 import Breadcrumbs from "./Breadcrumbs";
+import { useState } from "react";
 
 const ProductDetails = () => {
+  const [details, setDetails] = useState("Product");
+
   return (
     <section className="px-4 sm:px-[100px] max-container">
       <div className="border-b-2 " />
@@ -52,24 +55,82 @@ const ProductDetails = () => {
               <span className="opacity-50">/5</span>
             </p>
           </div>
+          {/* price */}
           <p className="font-satoshi font-bold text-[32px]">Price $</p>
+          {/* captions */}
           <p className="font-satoshi opacity-60">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
             inventore reprehenderit deserunt temporibus architecto, iure quos
             odio voluptatem{" "}
           </p>
+
           <div className="border-b-2 my-6" />
+          {/* availability */}
           <p className="font-satoshi opacity-60">Availability</p>
           <button className="py-3 px-6 mt-[16px] rounded-full ring-1 font-satoshi font-medium">
             Low, normal, hight
           </button>
           <div className="border-b-2 my-6" />
+          {/* quantity */}
           <button className="py-3 px-6 mt-[16px] w-full max-w-[170px] rounded-full ring-1 font-satoshi font-medium ">
             <span className="pr-5">-</span> 1 <span className="pl-5">+</span>
           </button>
           <button className="font-satoshi font-medium py-3 px-6 ml-[20px] ring-1 rounded-full w-full max-w-[400px]">
             Add to Cart
           </button>
+        </div>
+      </div>
+      {/* details + reviews */}
+      <div className="w-full">
+        {/* buttons */}
+        <div>
+          <button
+            onClick={() => setDetails("Product")}
+            className="py-6 w-[50%] border-b-2 font-satoshi text-xl opacity-60 hover:border-b-black hover:opacity-100 hover:font-bold transition ease-in-out duration-600"
+            style={
+              details === "Product"
+                ? {
+                    opacity: "1",
+                    fontWeight: "bold",
+                    borderBottom: "2px solid black",
+                  }
+                : undefined
+            }
+          >
+            Product Details
+          </button>
+          <button
+            onClick={() => setDetails("Reviews")}
+            className="mb-9 py-6 w-[50%] border-b-2 font-satoshi text-xl opacity-60 hover:border-b-black hover:opacity-100 hover:font-bold transition ease-in-out duration-600"
+            style={
+              details === "Reviews"
+                ? {
+                    opacity: "1",
+                    fontWeight: "bold",
+                    borderBottom: "2px solid black",
+                  }
+                : undefined
+            }
+          >
+            Rating & Reviews
+          </button>
+          <div className="mb-8 font-satoshi ">
+            {details === "Product" && (
+              <div className="text-xl space-y-2 opacity-60">
+                <p className=""> Brand: ...</p>
+                <p className=""> Weight: ...</p>
+                <ul className="">
+                  {" "}
+                  Dimensions
+                  <li>Width: ...</li>
+                  <li>Height: ...</li>
+                  <li>Depth: ...</li>
+                </ul>
+                <p>Warranty: ...</p>
+                <p>Shipping information: ...</p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </section>
