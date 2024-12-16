90308c4280e2d03a7e3d00a4dd927a214de8c953
Mateusz Obirek <neutralnie7@gmail.com>
2024-12-16 19:24:50 +0100

    added logic

--- app/src/components/Cart.tsx
+++ app/src/components/Cart.tsx
@@ -1,11 +1,33 @@
+import Footer from "../sections/Footer";
+import Newsletter from "../sections/Newsletter";
 import Breadcrumbs from "./Breadcrumbs";
 
 const Cart = () => {
   return (
-    <section className="max-container px-4 sm:px-[100px]">
-      <div className="border-b-2" />
-      <Breadcrumbs />
-    </section>
+    <>
+      {" "}
+      <section className="max-container px-4 sm:px-[100px]">
+        <div className="border-b-2" />
+        <Breadcrumbs />
+        <div>
+          <h2 className="mt-[24px] font-integralCFBold text-[40px] max-md:leading-[36px] sm:text-5xl">
+            your cart
+          </h2>
+          <div className="mt-[24px] flex gap-[20px]">
+            <div className="w-[715px] rounded-[20px] ring-1">
+              {/* purchased products */}
+            </div>
+            <div className="h-[458px] w-[505px] rounded-[20px] ring-1">
+              summary
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="max-container">
+        <Newsletter />
+        <Footer />
+      </div>
+    </>
   );
 };
 
