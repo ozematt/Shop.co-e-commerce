fac81619388088c1d1e6f3324d75d44547e00537
Mateusz Obirek <neutralnie7@gmail.com>
2024-12-16 19:36:48 +0100

    added style

--- app/src/components/Cart.tsx
+++ app/src/components/Cart.tsx
@@ -1,6 +1,8 @@
 import Footer from "../sections/Footer";
 import Newsletter from "../sections/Newsletter";
 import Breadcrumbs from "./Breadcrumbs";
+import deleteIcon from "../assets/Delete.svg";
+import QuantityButton from "./QuantityButton";
 
 const Cart = () => {
   return (
@@ -16,6 +18,20 @@ const Cart = () => {
           <div className="mt-[24px] flex gap-[20px]">
             <div className="w-[715px] rounded-[20px] ring-1">
               {/* purchased products */}
+              <div className="flex pl-[24px] pt-[20px]">
+                {/* IMG */}
+                <div className="h-[124px] w-[124px] rounded-lg ring-1" />
+                {/* product details */}
+                <div className="flex flex-col justify-between pl-[16px]">
+                  {" "}
+                  <h6 className="font-satoshi text-xl font-bold"> Title</h6>
+                  <p className="font-satoshi text-2xl font-bold">$ Price</p>
+                </div>
+                <div>
+                  <img src={deleteIcon} alt="" />
+                  <QuantityButton stock={1} />
+                </div>
+              </div>
             </div>
             <div className="h-[458px] w-[505px] rounded-[20px] ring-1">
               summary
