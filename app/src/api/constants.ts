67e976f82d83705922fd26d216e24e76ec7880af
Mateusz Obirek <neutralnie7@gmail.com>
2024-12-17 09:09:51 +0100

    added auth queries

--- app/src/api/constants.ts
+++ app/src/api/constants.ts
@@ -1,3 +1,4 @@
 export const CATEGORIES = "https://dummyjson.com/products/categories";
 export const CATEGORIES_LIST = "https://dummyjson.com/products/category-list";
 export const PRODUCTS = "https://dummyjson.com/products";
+export const AUTHORIZATION = "https://dummyjson.com/user/login";
