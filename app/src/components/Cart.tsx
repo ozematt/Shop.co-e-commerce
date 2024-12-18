6e23b2c9c91ae6bc52f4e767dfbbbb09f471ea9d
Mateusz Obirek <neutralnie7@gmail.com>
2024-12-18 09:41:29 +0100

    added summary style

--- app/src/components/Cart.tsx
+++ app/src/components/Cart.tsx
@@ -1,11 +1,12 @@
 import Footer from "../sections/Footer";
 import Newsletter from "../sections/Newsletter";
 import Breadcrumbs from "./Breadcrumbs";
-
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import { selectAllCart } from "../redux/cartSlice";
 import CartItem from "./CartItem";
+import discount from "../assets/Discount.svg";
+import arrowWhite from "../assets/Arrow_white.svg";
 
 const Cart = () => {
   const total = useSelector((state: RootState) => state.cart.total);
@@ -32,7 +33,67 @@ const Cart = () => {
               ))}
             </div>
 
-            <div className="w-[505px] rounded-[20px] ring-1">summary</div>
+            <div className="w-[505px] rounded-[20px] ring-1">
+              <div className="px-6 pb-[33px] pt-[20px]">
+                <h6 className="pb-1 font-satoshi text-2xl font-bold">
+                  Order Summary
+                </h6>
+                <div>
+                  <div className="flex justify-between pt-5">
+                    <p className="font-satoshi text-xl opacity-60">Subtotal </p>{" "}
+                    <p className="font-satoshi text-xl font-bold">$555</p>
+                  </div>
+
+                  <div className="flex justify-between pt-5">
+                    <p className="font-satoshi text-xl opacity-60">
+                      Discount <span>(-20%)</span>{" "}
+                    </p>{" "}
+                    <p className="font-satoshi text-xl font-bold text-red-500">
+                      -$100
+                    </p>
+                  </div>
+
+                  <div className="flex justify-between pt-5">
+                    <p className="font-satoshi text-xl opacity-60">
+                      Delivery Fee{" "}
+                    </p>{" "}
+                    <p className="font-satoshi text-xl font-bold">$15</p>
+                  </div>
+
+                  <div className="border-b-2 pt-5" />
+
+                  <div className="flex justify-between pt-5">
+                    <p className="font-satoshi text-xl">Total </p>{" "}
+                    <p className="font-satoshi text-2xl font-bold">$215</p>
+                  </div>
+
+                  <div className="relative flex justify-between pt-6">
+                    <img
+                      src={discount}
+                      alt="discount icon"
+                      className="absolute left-4 top-[50%] z-10 opacity-40"
+                    />
+                    <input
+                      type="text"
+                      placeholder="Add promo code"
+                      className="h-[48px] w-full max-w-[326px] rounded-full bg-grayBG pl-[50px] placeholder:opacity-40 focus:outline-none focus:ring-1 focus:ring-black"
+                    />{" "}
+                    <button className="rounded-full bg-black px-[38px] py-[13px] font-satoshi font-medium text-white transition duration-100 ease-in-out hover:scale-95">
+                      Apply
+                    </button>
+                  </div>
+
+                  <button className="relative mt-6 w-full max-w-[457px] rounded-full bg-black py-[19px] pr-9 font-satoshi font-medium text-white transition duration-100 ease-in-out hover:scale-95">
+                    Go to Checkout{" "}
+                    <img
+                      src={arrowWhite}
+                      alt="white arrow icon"
+                      className="absolute left-[60%] top-[32%]"
+                    />
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
