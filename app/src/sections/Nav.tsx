ba1ee24c373a7e3598a39d1f413ba4ec9ad6ca7b
Mateusz Obirek <neutralnie7@gmail.com>
2024-12-18 19:47:18 +0100

    added cart icon quantity

--- app/src/sections/Nav.tsx
+++ app/src/sections/Nav.tsx
@@ -1,11 +1,13 @@
 import { navLinks } from "../constants";
-import cartIcon from "../assets/Cart.png";
-import userIcon from "../assets/Avatar.png";
+import cartIcon from "../assets/Cart.svg";
+import userIcon from "../assets/Avatar.svg";
 import hamburger from "../assets/Hamburger.png";
 import lupeIcon from "../assets/Lupe_icon.png";
 import arrow from "../assets/Arrow down.png";
 import { useLocation, useNavigate } from "react-router";
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 
 const Nav = () => {
   //
@@ -15,6 +17,7 @@ const Nav = () => {
 
   const [open, setOpen] = useState(false);
   const auth = localStorage.getItem("user") || undefined;
+  const quantity = useSelector((state: RootState) => state.cart.itemsInCart);
 
   ////LOGIC
   const handleUserPanel = () => {
@@ -95,14 +98,24 @@ const Nav = () => {
           height={24}
           className="hidden cursor-pointer hover:opacity-60 max-[837px]:block"
         />
-        <img
-          src={cartIcon}
-          alt="cart icon"
-          width={24}
-          height={24}
-          onClick={handleCart}
-          className="cursor-pointer hover:opacity-60"
-        />
+        <div className="relative">
+          <img
+            src={cartIcon}
+            alt="cart icon"
+            width={24}
+            height={24}
+            onClick={handleCart}
+            className="cursor-pointer hover:opacity-60"
+          />
+          {quantity > 0 && (
+            <>
+              <div className="absolute right-[-7px] top-[-2px] h-[17px] w-[17px] rounded-full bg-orange-400 ring-2 ring-white" />
+              <p className="absolute right-[-2px] top-[-1.5px] font-satoshi text-[11px] font-medium text-black">
+                {quantity}
+              </p>
+            </>
+          )}
+        </div>
         <img
           src={userIcon}
           alt="user icon"
