2056e1ab88cbec3ff6df732b3ed1ecfb5608a0a0
Mateusz Obirek <neutralnie7@gmail.com>
2024-12-24 13:20:27 +0100

    added comment

--- app/src/components/Checkout.tsx
+++ app/src/components/Checkout.tsx
@@ -5,6 +5,20 @@ import Breadcrumbs from "./Breadcrumbs";
 import { UserAddress } from "../api/queries/user";
 
 const Checkout = () => {
+  // const [userAddress, setUserAddress] = useState<UserAddress | null>(null);
+
+  // const authUserData = localStorage.getItem("user");
+
+  // useEffect(() => {
+  //   if (authUserData) {
+  //     const user = JSON.parse(authUserData);
+  //     const authUserAddress: UserAddress = user.address;
+  //     setUserAddress(authUserAddress);
+  //   }
+  // }, [authUserData]);
+
+  // console.log(userAddress);
+
   return (
     <>
       <section className="max-container px-4 sm:px-[100px]">
