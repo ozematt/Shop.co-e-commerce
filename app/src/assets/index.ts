f29faa7ab122fb1de772273b232f676b0e1f9fce
Mateusz Obirek <neutralnie7@gmail.com>
2024-12-30 10:12:22 +0100

    added index graphic file

---
+++ app/src/assets/index.ts
@@ -0,0 +1,30 @@
+import arrow from "../assets/Arrow down.png";
+import discount from "../assets/Discount.svg";
+import arrowWhite from "../assets/Arrow_white.svg";
+import cartIcon from "../assets/Cart.svg";
+import deleteIcon from "../assets/Delete.svg";
+import minus from "../assets/Minus.svg";
+import plus from "../assets/Plus.svg";
+import tick from "../assets/Green tick.png";
+
+import settings from "../assets/Settings.png";
+import arrow from "../assets/Arrow down.png";
+import hamburger from "../assets/Hamburger.svg";
+
+import user from "../assets/User.png";
+import lock from "../assets/Lock.png";
+
+import arrowLeft from "../assets/Arrow-left.png";
+import arrowRight from "../assets/Arrow-right.png";
+
+import minus from "../assets/Minus.svg";
+import plus from "../assets/Plus.svg";
+
+import arrow from "../assets/Arrow down.png";
+
+import closeBlack from "../assets/Close_black.png";
+import settings from "../assets/Settings.png";
+
+import arrow from "../assets/Arrow down.png";
+
+import userIcon from "../assets/Avatar.svg";
