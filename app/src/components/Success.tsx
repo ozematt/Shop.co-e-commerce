b03b8b83016fcaa371976da2b81350cb440f0c30
Mateusz Obirek <neutralnie7@gmail.com>
2025-01-07 19:04:48 +0100

    added media queries

--- app/src/components/Success.tsx
+++ app/src/components/Success.tsx
@@ -7,16 +7,18 @@ const Success = () => {
 
   ////UI
   return (
-    <div className="ring-3 h-[270px] w-[500px] rounded-xl bg-white">
+    <div className="ring-3 mx-4 w-[500px] rounded-xl bg-white md:h-[270px]">
       <div className="flex flex-col px-4 py-9">
-        <h6 className="text-center font-integralCFBold text-4xl">Success!</h6>
-        <p className="pt-3 text-center font-satoshi text-lg">
+        <h6 className="text-center font-integralCFBold text-3xl md:text-4xl">
+          Success!
+        </h6>
+        <p className="pt-3 text-center font-satoshi text-lg max-md:text-sm">
           Thank you for your purchase! Your order has been successfully placed.
           You will be redirected to the homepage shortly.
         </p>
         <button
           onClick={() => navigate("/")}
-          className="mx-auto my-3 rounded-full bg-black px-[60px] py-3 font-satoshi text-white hover:scale-90 active:scale-100"
+          className="mx-auto my-3 rounded-full bg-black px-[60px] py-2 font-satoshi text-white hover:scale-90 active:scale-100 md:py-3"
         >
           OK
         </button>
