af4540df497dcf2ce7ffd65e3387fcf63f5c8e5b
Mateusz Obirek <neutralnie7@gmail.com>
2025-01-10 17:14:23 +0100

    added dark mode toggle fn

--- app/src/sections/Nav.tsx
+++ app/src/sections/Nav.tsx
@@ -15,6 +15,8 @@ const Nav = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
+  const handleThemToggle = () => {};
+
   ////UI
   return (
     <nav className="max-container flex h-[96px] items-center px-4 max-[838px]:justify-between sm:px-[100px]">
